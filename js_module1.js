9eac7efa1355ebf9b3aeb114d45d701bf0405cc0
myth <okmlnsunok@naver.com>
2019-07-23 09:45:22 +0900

    클로저1

--- js_module1.js
+++ js_module1.js
@@ -1,20 +1,8 @@
-function cal(mode){
-    var funcs = {
-        'plus' : function(left, right){return left + right},
-        'minus' : function(left, right){return left - right}
+function outter(){
+    function inner(){
+        var title = 'coding everybody'; 
+        alert(title);
     }
-    return funcs[mode];
+    inner();
 }
-alert(cal('plus')(2,1));
-alert(cal('minus')(2,1));
-
-var process = [
-    function(input){ return input + 10;},
-    function(input){ return input * input;},
-    function(input){ return input / 2;}
-];
-var input = 1;
-for(var i = 0; i < process.length; i++){
-    input = process[i](input);
-}
-alert(input);
+outter();
