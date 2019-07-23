382be88d69102ac77d015add105cf538801a6f0e
myth <okmlnsunok@naver.com>
2019-07-23 10:47:31 +0900

    클로저4

--- js_module1.js
+++ js_module1.js
@@ -1,20 +1,11 @@
-function factory_movie(title){
-    return {
-        get_title : function (){
-            return title;
-        },
-        set_title : function(_title){
-            title = _title
+var arr = []
+for(var i = 0; i < 5; i++){
+    arr[i] = function(id) {
+        return function(){
+            return id;
         }
-    }
+    }(i);
 }
-ghost = factory_movie('Ghost in the shell');
-matrix = factory_movie('Matrix');
- 
-alert(ghost.get_title());
-alert(matrix.get_title());
- 
-ghost.set_title('공각기동대');
- 
-alert(ghost.get_title());
-alert(matrix.get_title());
+for(var index in arr) {
+    console.log(arr[index]());
+}
