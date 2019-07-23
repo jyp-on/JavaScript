c3635c7c119280f2f87787d1a89e38eb5299fc33
myth <okmlnsunok@naver.com>
2019-07-23 09:58:47 +0900

    클로저3

--- js_module1.js
+++ js_module1.js
@@ -1,8 +1,20 @@
-function outter(){
-    var title = 'coding everybody';  
-    return function(){        
-        alert(title);
+function factory_movie(title){
+    return {
+        get_title : function (){
+            return title;
+        },
+        set_title : function(_title){
+            title = _title
+        }
     }
 }
-inner = outter();
-inner();
+ghost = factory_movie('Ghost in the shell');
+matrix = factory_movie('Matrix');
+ 
+alert(ghost.get_title());
+alert(matrix.get_title());
+ 
+ghost.set_title('공각기동대');
+ 
+alert(ghost.get_title());
+alert(matrix.get_title());
